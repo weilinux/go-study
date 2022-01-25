27b4b158771c67ba162a9fc2ed4f5e7d12476de3
lxz <18007360791@163.com>
2022-01-25 15:22:52 +0800

    排序 冒泡排序，案例：将数组中的元素（数字）按升序或降序的方式排列 效率最慢

--- 数据结构与算法/排序/冒泡排序/bubbleSort.go
+++ 数据结构与算法/排序/冒泡排序/bubbleSort.go
@@ -9,15 +9,18 @@ import (
 func sort() {
 	start := time.Now()
 
-	arr := make([]int, 10000)
+	arr := make([]int, 100)
 	rand.Seed(start.Unix())
-	for s := 0; s < 10000; s++ {
-		arr[s] = rand.Intn(10000)
+	for s := 0; s < 100; s++ {
+		arr[s] = rand.Intn(100)
 	}
 	fmt.Printf("arr 排序前：%v\n", arr)
 
+	//第一次循环遍历元素的个数（需要循环多少遍）
 	for i := 0; i < len(arr)-1; i++ {
+		//第二次循环遍历需要调整的个数（需要调整多少次，由于每次循环都能调整出当前循环体內最大的数所以第二遍循环需要遍历的个数每次都可以-1（-i））
 		for j := 0; j < len(arr)-1-i; j++ {
+			//如果当前元素大于索引下一位的元素则双方调换位置
 			if arr[j] > arr[j+1] {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
 			}
