f5fe5e9b683ed5434c8a03f11d4d31ff80d2403a
lxz <18007360791@163.com>
2022-02-28 15:25:00 +0800

    mysql MySQL是一个关系型数据库管理系统，由瑞典MySQL AB 公司开发，属于 Oracle 旗下产品。MySQL 是最流行的关系型数据库管理系统之一，在 WEB 应用方面，MySQL是最好的 RDBMS (Relational Database Management System，关系数据库管理系统) 应用软件之一

--- 数据库/关系型/mysql/mySQL.go
+++ 数据库/关系型/mysql/mySQL.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"time"
 )
 
 //上下文
@@ -30,6 +31,27 @@ func main() {
 
 	//持续会话模式通常被用于执行一系列操作
 	tx := db.WithContext(ctx)
+
+	// 获取通用数据库对象 sql.DB，然后使用其提供的功能
+	sqlDB, err := tx.DB()
+
+	// Ping
+	sqlDB.Ping()
+
+	// Close
+	defer sqlDB.Close()
+
+	// 返回数据库统计信息
+	sqlDB.Stats()
+
+	// SetMaxIdleConns 用于设置连接池中空闲连接的最大数量。
+	sqlDB.SetMaxIdleConns(10)
+
+	// SetMaxOpenConns 设置打开数据库连接的最大数量。
+	sqlDB.SetMaxOpenConns(100)
+
+	// SetConnMaxLifetime 设置了连接可复用的最大时间。
+	sqlDB.SetConnMaxLifetime(time.Hour)
 
 	//CURD...
 	tx.Create("")
