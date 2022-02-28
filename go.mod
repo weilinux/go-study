a28c01a3afdcb509be63d0c08d5dccc5b83d546c
lxz <18007360791@163.com>
2022-02-28 14:29:37 +0800

    mysql MySQL是一个关系型数据库管理系统，由瑞典MySQL AB 公司开发，属于 Oracle 旗下产品。MySQL 是最流行的关系型数据库管理系统之一，在 WEB 应用方面，MySQL是最好的 RDBMS (Relational Database Management System，关系数据库管理系统) 应用软件之一

--- go.mod
+++ go.mod
@@ -13,13 +13,17 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
 	github.com/go-redis/redis/v8 v8.11.4 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/gorilla/sessions v1.2.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-sqlite3 v1.14.11 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/ugorji/go/codec v1.2.6 // indirect
@@ -28,4 +32,7 @@ require (
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gorm.io/driver/mysql v1.3.2 // indirect
+	gorm.io/driver/sqlite v1.3.1 // indirect
+	gorm.io/gorm v1.23.1 // indirect
 )
---
+++ 数据库/关系型/mysql/mysql.go
@@ -0,0 +1,40 @@
+package mysql
+
+import (
+	"context"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var ctx = context.Background()
+
+func main() {
+	//TODO 使用gorm操作
+	//go get -u gorm.io/gorm
+	//go get -u gorm.io/driver/sqlite
+	//go get gorm.io/driver/mysql
+
+	//连接mysql
+	db, err := gorm.Open(mysql.New(mysql.Config{ //自定义驱动
+		DSN:                       "gorm:gorm@tcp(127.0.0.1:3306)/gorm?charset=utf8&parseTime=True&loc=Local", // DSN data source name
+		DefaultStringSize:         256,                                                                        // string 类型字段的默认长度
+		DisableDatetimePrecision:  true,                                                                       // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
+		DontSupportRenameIndex:    true,                                                                       // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
+		DontSupportRenameColumn:   true,                                                                       // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
+		SkipInitializeWithVersion: false,                                                                      // 根据当前 MySQL 版本自动配置
+	}), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	//持续会话模式通常被用于执行一系列操作
+	tx := db.WithContext(ctx)
+
+	//CURD...
+	tx.Create("")
+	tx.Find("")
+	tx.Updates("")
+	tx.Delete("")
+
+	//详细操作观看：https://gorm.io/zh_CN/docs/v2_release_note.html
+}
