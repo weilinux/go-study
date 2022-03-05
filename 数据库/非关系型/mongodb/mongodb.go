e88aea360aafb0a5c9c6725afbada875ed44a310
liaoxingzhi <42024275+liaoxingzhi@users.noreply.github.com>
2022-03-05 21:16:12 +0800

    mongodb MongoDB是一个基于分布式文件存储的数据库。由C++语言编写。旨在为WEB应用提供可扩展的高性能数据存储解决方案

--- 数据库/非关系型/mongodb/mongodb.go
+++ 数据库/非关系型/mongodb/mongodb.go
@@ -4,25 +4,25 @@ import (
 	"context"
 	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/x/bsonx"
 	"log"
-	"time"
 )
 
-// ConnectToDB 连接池模式
-func ConnectToDB(uri, name string, timeout time.Duration, num uint64) (*mongo.Database, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
-	o := options.Client().ApplyURI(uri)
-	o.SetMaxPoolSize(num)
-	client, err := mongo.Connect(ctx, o)
-	if err != nil {
-		return nil, err
-	}
-
-	return client.Database(name), nil
+// User 模型
+type User struct {
+	Id  primitive.ObjectID `bson:"_id"`
+	Ips []Ip               `bson:"ips"`
+	Key string             `bson:"key"`
 }
+
+type Ip struct {
+	Ip string `bson:"ip"`
+}
+
+var ctx = context.Background()
 
 func main() {
 	//TODO 使用mongo-driver操作
@@ -35,32 +35,116 @@ func main() {
 	//E：D里面的一个元素。
 
 	// 设置客户端连接配置
-	clientOptions := options.Client().ApplyURI("mongodb://192.168.17.128:27017")
+	clientOptions := options.Client().ApplyURI("mongodb://192.168.17.128:27017").SetAuth(options.Credential{
+		AuthSource: "database", //用于身份验证的数据库名 （admin）
+		Username:   "username", //用户名
+		Password:   "password", //密码
+	})
+
+	// 设置连接池最大连接数
+	clientOptions.SetMaxPoolSize(100)
+
+	// 设置连接池同时建立的最大连接数
+	clientOptions.SetMaxConnecting(10)
 
 	// 连接到MongoDB
-	client, err := mongo.Connect(context.TODO(), clientOptions)
+	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// 检查连接
-	err = client.Ping(context.TODO(), nil)
+	err = client.Ping(ctx, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// 断开连接
-	err = client.Disconnect(context.TODO())
+	err = client.Disconnect(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// 指定操作的数据库名
+	database := client.Database("cbec_oc")
+
 	// 指定获取要操作的数据集
-	collection := client.Database("cbec_oc").Collection("inventory_sku")
+	collection := database.Collection("inventory_sku")
+
+	// 设置索引
+	idx := mongo.IndexModel{
+		Keys:    bsonx.Doc{{"name", bsonx.Int32(1)}},
+		Options: options.Index().SetUnique(true),
+	}
+	idxRet, err := collection.Indexes().CreateOne(context.Background(), idx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("collection.Indexes().CreateOne:", idxRet)
+
+	// 事务 只有闭包方式才能使用事务
+	if err = client.UseSession(ctx, func(sessionContext mongo.SessionContext) error {
+		// 开启事务
+		err := sessionContext.StartTransaction()
+		if err != nil {
+			return err
+		}
+
+		// 事务内使用读写操作必须传入一个带session-value的context对象 （sessionContext）
+		_, err = collection.InsertOne(sessionContext, bson.M{"name": 13, "password": "123456"})
+		if err != nil {
+			return err
+		}
+
+		_, err = collection.InsertOne(sessionContext, bson.M{"name": 14, "password": "123456"})
+		if err != nil {
+			// 事务回滚
+			err := sessionContext.AbortTransaction(sessionContext)
+			if err != nil {
+				return err
+			}
+			return err
+		} else {
+			// 事务提交
+			err := sessionContext.CommitTransaction(sessionContext)
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}); err != nil {
+		log.Fatal(err)
+	}
+
+	//创建事务session
+	session, err := client.StartSession()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//关闭事务session
+	defer session.EndSession(ctx)
+
+	//插入一条数据
+	_, err = collection.InsertOne(ctx, "")
+	if err != nil {
+		//事务回滚
+		err := session.AbortTransaction(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		//事务提交
+		err := session.CommitTransaction(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 
 	//CURD...
 	//插入单条
-	insertOneResult, err := collection.InsertOne(context.TODO(), "")
+	insertOneResult, err := collection.InsertOne(ctx, "")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -75,7 +159,7 @@ func main() {
 			{"classifyData.initial", 1},
 		}},
 	}
-	updateResult, err := collection.UpdateOne(context.TODO(), filter, update)
+	updateResult, err := collection.UpdateOne(ctx, filter, update)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -86,10 +170,9 @@ func main() {
 	//updateResult, err := collection.UpdateMany()
 
 	//查询单条
-	type InventorySku struct{}
-	var inventorySku InventorySku
+	var user *User
 	//将结果绑定到结构体中
-	err = collection.FindOne(context.TODO(), filter).Decode(&inventorySku)
+	err = collection.FindOne(ctx, filter).Decode(&user)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -98,31 +181,30 @@ func main() {
 	findOptions.SetLimit(2) //更多配置需详查源代码
 	//查询多条
 	//把bson.D{{}}作为一个filter可以匹配所有文档
-	cursor, err := collection.Find(context.TODO(), filter, findOptions)
+	cursor, err := collection.Find(ctx, filter, findOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
-	for cursor.Next(context.TODO()) {
+	for cursor.Next(ctx) {
 		// 创建一个值，将单个文档解码为该值
-		var inventorySku InventorySku
-		var results []*InventorySku
-		err := cursor.Decode(&inventorySku)
+		var results []*User
+		err := cursor.Decode(&user)
 		if err != nil {
 			log.Fatal(err)
 		}
-		results = append(results, &inventorySku)
+		results = append(results, user)
 	}
 	if err := cursor.Err(); err != nil {
 		log.Fatal(err)
 	}
 	// 完成后关闭游标
-	err = cursor.Close(context.TODO())
+	err = cursor.Close(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	//删除单条
-	deleteResult, err := collection.DeleteOne(context.TODO(), filter)
+	deleteResult, err := collection.DeleteOne(ctx, filter)
 	if err != nil {
 		log.Fatal(err)
 	}
