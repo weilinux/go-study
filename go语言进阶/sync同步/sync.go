9f2143b14314e49b61605dbb72fc531a1f16b23c
liaoxingzhi <42024275+liaoxingzhi@users.noreply.github.com>
2022-03-15 14:45:37 +0800

    sync包在并发编程中锁的主要作用是保证多个线程或者goroutine在访问同一片内存时不会出现混乱

--- go语言进阶/sync同步/sync.go
+++ go语言进阶/sync同步/sync.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
+	"time"
 )
 
 func main() {
@@ -68,15 +70,60 @@ func main() {
 	// 解锁(读操作)
 	locker.Unlock()
 
-	// sync.Cond实现了一个条件变量，用于等待一个或一组goroutines满足条件后唤醒的场景
-	var cond sync.Cond
-	// 挂起当前执行的goroutine （自动调用cond.L.Lock()上锁方法）
+	// sync.Cond实现了一个条件变量，用于等待一个或一组goroutines满足条件后唤起的场景，每个Cond都有一个关联的Locker L（通常是* Mutex或* RWMutex）
+	// 另外一种写法创建：cond := &sync.Cond{L: new(sync.Mutex)}
+	cond := sync.NewCond(new(sync.Mutex))
+	// 阻塞当前的goroutine，等待唤起 必须遵守以下写法
+	//    c.L.Lock() 上锁
+	//    for !condition() { 不满足条件则阻塞goroutine执行
+	//        c.Wait() 阻塞
+	//    }
+	//    ... make use of condition ... 利用条件（阻塞结束后业务逻辑...）
+	//    c.L.Unlock() 解锁
 	cond.Wait()
-	// 唤醒所有被挂起的goroutine （自动调用cond.L.Unlock()解锁方法）
+	// 唤起所有阻塞的goroutine
 	cond.Broadcast()
-	// 唤醒第一个被挂起的goroutine
+	// 唤起第一个阻塞的goroutine
 	cond.Signal()
+	// 上锁
+	cond.L.Lock()
+	// 解锁
+	cond.L.Unlock()
 
-	/*var once sync.Once
-	var pool sync.Pool*/
+	// sync.Once是Golang package中使方法只执行一次的对象实现，作用与init函数类似。但也有所不同
+	// init函数是在文件包首次被加载的时候执行，且只执行一次
+	// sync.Onc是在代码运行中需要的时候执行，且只执行一次
+	var once sync.Once
+	// 自定义函数变量
+	funcOne := func() { fmt.Println("函数只会被执行一次") }
+	for i := 0; i < 3; i++ {
+		// 执行函数 不管是在循环中还是多个协程中都只会被执行一次
+		once.Do(funcOne)
+	}
+	// sync.Pool是对象缓存池，用来减少堆上内存的反复申请和释放的
+	var pool = sync.Pool{
+		// New方法定义默认的数据对象，如果没有设置则返回nil
+		New: func() interface{} {
+			return "default data"
+		},
+	}
+	// Get方法从pool对象缓存池里获取任意一个对象 步骤：
+	// 1.private不是空的，那就直接拿来用
+	// 2.private是空的，那就先去本地的shared队列里面从头 pop 一个
+	// 3.本地的shared也没有了，那getSlow去拿，其实就是去别的P的shared里面偷，偷不到就去victim幸存者里面找
+	// 4.最后都没有，那就只能调用New方法创建一个了
+	fmt.Println(pool.Get().(string)) // 因为返回值是interface{}，因此需要进行类型转换
+	// Put方法将数据对象放进pool对象缓存池 步骤：
+	// 1.private没有，就放在private
+	// 2.private有了，那么就放到shared队列的头部
+	pool.Put("put data")
+	// 第一次GC垃圾回收会将pool对象缓存池内数据拷贝一份放进victim幸存者中,避免GC将其清空
+	runtime.GC()
+	time.Sleep(1 * time.Second)
+	fmt.Println(pool.Get().(string)) // put data | default data 因为Get方法是获取任意一个对象，所以此处应有两种结果
+	fmt.Println(pool.Get().(string)) // default data
+	// 每个sync.Pool的生命周期为两次GC中间时段才有效 （最多存放两次GC）
+	runtime.GC()
+	time.Sleep(1 * time.Second)
+	fmt.Println(pool.Get().(string)) // default data
 }
